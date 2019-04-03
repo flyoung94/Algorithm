77098df22d75c309ab2acebc06abed029f8cca8c
flyoung94 <834677043@qq.com>
2019-04-03 21:47:51 +0800

    version:2.0

--- Greedy/Subsequene.cpp
+++ Greedy/Subsequene.cpp
@@ -21,7 +21,7 @@ public:
 		int Status = Begin;  
 		if(nums.empty())
 			return 0;
-																        for(int i=0; i<nums.size()-1;i++){
+		for(int i=0; i<nums.size()-1;i++){
 			switch(Status){
 				case Begin: {
 					if(nums[i+1] > nums[i]){
